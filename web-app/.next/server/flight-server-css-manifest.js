1ec2472e699a0fe142811741d10d96ba2295a693
Andrei Ce <andrei90@gmail.com>
2023-05-18 18:09:41 -0300

    init commit: setup BE, FE & mobile

---
+++ web-app/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/andreice/Documents/0_code/15_rocketseat/nlw-timecapsule/web-app/src/app/layout.tsx": [
+      "/Users/andreice/Documents/0_code/15_rocketseat/nlw-timecapsule/web-app/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/andreice/Documents/0_code/15_rocketseat/nlw-timecapsule/web-app/src/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/andreice/Documents/0_code/15_rocketseat/nlw-timecapsule/web-app/src/app/page": [
+      "/Users/andreice/Documents/0_code/15_rocketseat/nlw-timecapsule/web-app/src/app/globals.css",
+      "/Users/andreice/Documents/0_code/15_rocketseat/nlw-timecapsule/web-app/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
---
+++ web-app/src/app/ignite/page.tsx
@@ -0,0 +1,3 @@
+export default function Ignite() {
+  return <div>Ignite</div>
+}
